a6197955d4fd6bc86c8c9b160e9f91f496dfd1b9
e-karch <karchemil@gmail.com>
2025-05-25 14:00:06 +0200

    Skeleton Spring Boot backend

---
+++ src/test/java/com/backend/nutricheck/NutricheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.nutricheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
