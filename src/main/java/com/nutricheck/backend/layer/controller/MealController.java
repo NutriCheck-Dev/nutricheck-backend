6ff1c70ef30990057af977c9e533604832e9f675
Moritz Münter <mmuenter3004@gmail.com>
2025-06-20 21:49:20 +0200

    Recipe and Meal Controller methods structure

--- src/main/java/com/nutricheck/backend/layer/controller/MealController.java
+++ src/main/java/com/nutricheck/backend/layer/controller/MealController.java
@@ -1,8 +1,15 @@
 package com.nutricheck.backend.layer.controller;
 
+import com.nutricheck.backend.dto.FoodProductDTO;
+import com.nutricheck.backend.dto.RecipeDTO;
 import com.nutricheck.backend.layer.service.MealService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("nutricheck/user/search/")
@@ -12,5 +19,15 @@ public class MealController {
 
     public MealController(MealService mealService) {
         this.mealService = mealService;
+    }
+
+    @GetMapping("/product/{name}")
+    public ResponseEntity<List<FoodProductDTO>> searchFoodProduct(@PathVariable String name) {
+        return null;
+    }
+
+    @GetMapping("/recipe/{name}")
+    public ResponseEntity<List<RecipeDTO>> searchRecipe(@PathVariable String name) {
+        return null;
     }
 }
--- src/main/java/com/nutricheck/backend/layer/controller/RecipeController.java
+++ src/main/java/com/nutricheck/backend/layer/controller/RecipeController.java
@@ -1,8 +1,10 @@
 package com.nutricheck.backend.layer.controller;
 
+import com.nutricheck.backend.dto.RecipeDTO;
+import com.nutricheck.backend.dto.ReportDTO;
 import com.nutricheck.backend.layer.service.RecipeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("nutricheck/user/recipe/")
@@ -12,5 +14,25 @@ public class RecipeController {
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<RecipeDTO> uploadRecipe(@RequestBody RecipeDTO recipeDTO) {
+        return null;
+    }
+
+    @PostMapping("/report")
+    public ResponseEntity<ReportDTO> reportRecipe(@RequestBody ReportDTO reportDTO) {
+        return null;
+    }
+
+    @PostMapping("/rate/{recipeId}/{rating}")
+    public ResponseEntity<RecipeDTO> rateRecipe(@PathVariable Long recipeId, @PathVariable int rating) {
+        return null;
+    }
+
+    @GetMapping("/download/{recipeId}")
+    public ResponseEntity<RecipeDTO> downloadRecipe(@PathVariable Long recipeId) {
+        return null;
     }
 }
