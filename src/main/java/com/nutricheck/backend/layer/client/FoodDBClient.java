8401601782a01bb4684b595a280f7d74a5d18afb
Moritz Münter <mmuenter3004@gmail.com>
2025-06-24 15:36:16 +0200

    Open Food Facts API calls with Alper Mulayim

--- src/main/java/com/nutricheck/backend/layer/client/FoodDBClient.java
+++ src/main/java/com/nutricheck/backend/layer/client/FoodDBClient.java
@@ -1,4 +1,10 @@
 package com.nutricheck.backend.layer.client;
 
+import com.alpermulayim.openfoodfacts_spring_boot_starter.requests.ProductSearchRequest;
+import com.alpermulayim.openfoodfacts_spring_boot_starter.responses.OpenFoodFactsPageResponse;
+
 public interface FoodDBClient {
+
+    OpenFoodFactsPageResponse search(ProductSearchRequest request);
+
 }
--- src/main/java/com/nutricheck/backend/layer/client/OpenFoodFactsClient.java
+++ src/main/java/com/nutricheck/backend/layer/client/OpenFoodFactsClient.java
@@ -1,7 +1,21 @@
 package com.nutricheck.backend.layer.client;
 
+import com.alpermulayim.openfoodfacts_spring_boot_starter.OpenFoodFactsApi;
+import com.alpermulayim.openfoodfacts_spring_boot_starter.requests.ProductSearchRequest;
+import com.alpermulayim.openfoodfacts_spring_boot_starter.responses.OpenFoodFactsPageResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OpenFoodFactsClient implements FoodDBClient {
+
+    private final OpenFoodFactsApi webClient;
+
+    public OpenFoodFactsClient(OpenFoodFactsApi webClient) {
+        this.webClient = webClient;
+    }
+
+    @Override
+    public OpenFoodFactsPageResponse search(ProductSearchRequest request) {
+        return null;
+    }
 }
