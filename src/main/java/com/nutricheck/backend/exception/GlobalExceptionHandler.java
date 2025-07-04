ba2334093195ea85983c7aa02f34992ac5d2ef4b
Emil Karch <karchemil@gmail.com>
2025-07-04 11:17:47 +0200

    Implement exception package

--- src/main/java/com/nutricheck/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nutricheck/backend/exception/GlobalExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.nutricheck.backend.exception;
 
 import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,24 +11,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return null; // get problem detail from exception and return it
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder(ex, ex.getBody()).build();
+        return ResponseEntity.badRequest().body(errorResponse);
     }
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public ResponseEntity<ProblemDetail> handleConstraintViolationException(ConstraintViolationException ex) {
-        return null; // construct problem detail
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
     @ExceptionHandler(value = DuplicateRecipeException.class)
-    public ResponseEntity<ProblemDetail> handleDuplicateRecipeException(DuplicateRecipeException ex) {
-        return null; // construct problem detail
+    public ResponseEntity<ErrorResponse> handleDuplicateRecipeException(DuplicateRecipeException ex) {
+        ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.CONFLICT, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
     @ExceptionHandler(value = RecipeNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleRecipeNotFoundException(RecipeNotFoundException ex) {
-        return null; // construct problem detail
+    public ResponseEntity<ErrorResponse> handleRecipeNotFoundException(RecipeNotFoundException ex) {
+        ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
     @ExceptionHandler(value = ReportNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleReportNotFoundException(ReportNotFoundException ex) {
-        return null; // construct problem detail
+    public ResponseEntity<ErrorResponse> handleReportNotFoundException(ReportNotFoundException ex) {
+        ErrorResponse errorResponse = ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
