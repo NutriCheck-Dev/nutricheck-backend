c7cc246d31dbb8169491fcb2274ffc6051dcb96b
Emil Karch <karchemil@gmail.com>
2025-07-19 21:41:13 +0200

    Fix issue with gemini api key

--- src/main/java/com/nutricheck/backend/layer/client/GeminiClient.java
+++ src/main/java/com/nutricheck/backend/layer/client/GeminiClient.java
@@ -1,17 +1,21 @@
 package com.nutricheck.backend.layer.client;
 
+
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentConfig;
 import com.google.genai.types.Schema;
 import com.nutricheck.backend.dto.AIMealDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GeminiClient implements AIModelClient {
     private final Client geminiClient;
 
-    public GeminiClient() {
-        this.geminiClient = new Client();
+    public GeminiClient(@Value("${gemini.api.key}") String geminiApiKey) {
+        this.geminiClient = Client.builder()
+                .apiKey(geminiApiKey)
+                .build();
     }
     @Override
     public AIMealDTO estimateMeal(byte[] image) {
